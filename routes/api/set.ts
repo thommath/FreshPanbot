a41f0a0f847c264ce3bbe90b1f5887f7a4ce43c2
Maria Nylund <mariaemnylund@gmail.com>
2023-02-03 17:10:17 +0100

    remove key when setting value

--- routes/api/set.ts
+++ routes/api/set.ts
@@ -3,7 +3,7 @@ import { redis, REDIS_QUEUE_KEY } from "./redis.ts";
 
 export const handler: Handlers = {
   async POST(req) {
-    await (await redis).ltrim(REDIS_QUEUE_KEY, -1, 0);
+    await (await redis).del(REDIS_QUEUE_KEY);
     await (await redis).lpush(REDIS_QUEUE_KEY, await req.text());
     return new Response("ok");
   }
