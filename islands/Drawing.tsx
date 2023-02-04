143fe2752d9e8dc03420897e485e1a272ac32fa7
Maria Nylund <mariaemnylund@gmail.com>
2023-02-04 11:24:33 +0100

    200 size

--- islands/Drawing.tsx
+++ islands/Drawing.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "preact/hooks";
 
 type Stroke = { x: number; y: number }[];
 
-const size = 300;
+const size = 200;
 
 export default function DrawingComponent() {
   const [isDrawing, setIsDrawing] = useState(false);
@@ -151,7 +151,7 @@ export default function DrawingComponent() {
       <path
         key={index}
         d={strokeToPath(stroke)}
-        stroke-width="8"
+        stroke-width="6"
         stroke-linecap="round"
         style={{ fill: "none", stroke: "black" }}
       />
