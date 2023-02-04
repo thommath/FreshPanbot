c1c7e61d996c8e18bdf05f7b5adc0b28b17c5f3b
Maria Nylund <mariaemnylund@gmail.com>
2023-02-04 10:32:45 +0100

    implemented long pulling

--- routes/api/get.ts
+++ routes/api/get.ts
@@ -1,9 +1,17 @@
 import { Handlers } from "$fresh/server.ts";
 import { redis, REDIS_QUEUE_KEY } from "./redis.ts";
 
+const timeout = 25;
+const pull_interval = 100;
 export const handler: Handlers = {
   async GET() {
-    const text = await (await redis).lpop(REDIS_QUEUE_KEY);
+    const getText = async () => await (await redis).lpop(REDIS_QUEUE_KEY);
+
+    let text = null;
+    for(let counter = 0; counter < (timeout*1000/pull_interval) && !text; counter++) {
+        text = await getText();
+        await new Promise(res => setTimeout(res, pull_interval));
+    }
     return new Response(text);
   }
 };
