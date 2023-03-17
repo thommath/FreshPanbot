75418215516e4df7e0e6f043dff7883a6a014325
Maria Nylund <mariaemnylund@gmail.com>
2023-03-17 13:39:17 +0100

    logo and wider base

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,55 @@
+import { asset } from "$fresh/runtime.ts";
 
 export default function Header(props: { selectedTab: number }) {
   return (
     <>
-      <h1 class="text-4xl text-center pb-4">Panbot</h1>
+      <div class="inline-flex">
+        <img src={asset("logo.png")} alt="" width="6rem" />
+        <h1 class="text-4xl text-center pb-4">Panbot</h1>
+      </div>
       <div class="flex justify-between">
         <a class="w-full" href="/">
-          <button class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${props.selectedTab === 0 ? '600' : '500'}`}>
+          <button
+            class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${
+              props.selectedTab === 0 ? "600" : "500"
+            }`}
+          >
             Preset
           </button>
         </a>
         <a class="w-full" href="/image">
-          <button class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${props.selectedTab === 1 ? '600' : '500'}`}>
+          <button
+            class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${
+              props.selectedTab === 1 ? "600" : "500"
+            }`}
+          >
             Image
           </button>
         </a>
         <a class="w-full" href="/drawing">
-          <button class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${props.selectedTab === 2 ? '600' : '500'}`}>
+          <button
+            class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${
+              props.selectedTab === 2 ? "600" : "500"
+            }`}
+          >
             Drawing
           </button>
         </a>
         <a class="w-full" href="/printQueue">
-          <button class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${props.selectedTab === 3 ? '600' : '500'}`}>
+          <button
+            class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${
+              props.selectedTab === 3 ? "600" : "500"
+            }`}
+          >
             Print queue
           </button>
         </a>
         <a class="w-full" href="/history">
-          <button class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${props.selectedTab === 3 ? '600' : '500'}`}>
+          <button
+            class={`w-full p-2 bg-yellow-500 border-2 border-yellow-${
+              props.selectedTab === 3 ? "600" : "500"
+            }`}
+          >
             History
           </button>
         </a>
